4ac7dc94d1ce0d13a072aa5426f916450da41bde
oWiCo <lesyukruslan@gmail.com>
2023-07-05 14:36:20 +0300

    make some changes in the code

--- calculator.py
+++ calculator.py
@@ -1,4 +1,17 @@
 #!/bin/env python3
+colors = {
+    'black': '\033[30m',
+    'red': '\033[31m',
+    'green': '\033[32m',
+    'yellow': '\033[33m',
+    'blue': '\033[34m',
+    'magenta': '\033[35m',
+    'cyan': '\033[36m',
+    'white': '\033[37m',
+    'reset': '\033[0m'
+}
+
+
 
 def addition(num1, num2):
     return num1 + num2
@@ -19,19 +32,19 @@ print("Welcome to the Calculator Program!")
 
 while True:
     print("\nPlease select an operation:")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
-    print("5. Exit")
+    print(colors['green'] + "1. Addition" + colors['reset']) 
+    print(colors['green'] + "2. Subtraction" + colors['reset'])
+    print(colors['green'] + "3. Multiplication" + colors['reset'])
+    print(colors['green'] + "4. Division" + colors['reset'])
+    print(colors['magenta'] + "5. Exit" + colors['reset'])
 
     choice = input("Enter your choice (1-5): ")
 
     if choice == "5":
-        print("Exiting the calculator. Goodbye!")
+        print(colors['cyan'] + "Exiting the calculator. Goodbye!" + colors['reset'])
         break
     elif choice not in ["1", "2", "3", "4"]:
-        print("Invalid choice. Please try again.")
+        print(colors['red'] + "Invalid choice. Please try again." + colors['reset'])
         continue
     
     
@@ -40,7 +53,7 @@ while True:
             num1 = float(input("Enter the first number: "))
             break
         except ValueError:
-            print("Invalid input. Please enter a valid number for the first number, for example (1,2,3,11,12,13,111,222,333).")
+            print(colors['red'] + "Invalid input. For example you need to type Integer like: (1, 2, 3,...) or Float like: (1.1, 1.2, 1.3,... )" + colors['reset'])
             
 
     while True:
@@ -48,22 +61,22 @@ while True:
             num2 = float(input("Enter the second number: "))
             break
         except ValueError:
-            print("Invalid input. Please enter a valid number for the second number, for example (1,2,3,11,12,13,111,222,333).")
+            print(colors['red'] + "Invalid input. For example you need to type Integer like: (1, 2, 3,...) or Float like: (1.1, 1.2, 1.3,... )" + colors['reset'])
           
             
            
     if choice == "1":
         result = addition(num1, num2)
-        print("Result:", result)
+        print(colors['yellow'] + "Result:" + colors['reset'], result)
     elif choice == "2":
         result = subtraction(num1, num2)
-        print("Result:", result)
+        print(colors['yellow'] + "Result:" + colors['reset'], result)
     elif choice == "3":
         result = multiplication(num1, num2)
-        print("Result:", result)
+        print(colors['yellow'] + "Result:" + colors['reset'], result)
     elif choice == "4":
         result = division(num1, num2)
-        print("Result:", result)
+        print(colors['yellow'] + "Result:" + colors['reset'], result)
     else:
-        print("Invalid choice. Please try again.")
+        print(colors['red'] + "Sorry something went wrong, try again..." + colors['reset'])
 
